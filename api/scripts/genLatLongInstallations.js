58b4de5a73d7d377750705566b7e8926eed093fc
Andrei Tudor <andrei.tudor27@gmail.com>
2021-10-20 03:48:45 +0200

    Gen Lat Long

--- api/scripts/genLatLongInstallations.js
+++ api/scripts/genLatLongInstallations.js
@@ -16,8 +16,8 @@ let getInstallationsQuery = `SELECT * from installation`
 let updateInstallationsQuery = `UPDATE installation
 								SET
 								lat=?,
-								long=?
-								where uuid=?`;
+								\`long\`=?
+								where uuid=?`
 
 router.get('/gen-lat-long', async (req, res) => {
 	let installations = await mysqlConn.query(getInstallationsQuery).then(rs => rs[0])
@@ -26,7 +26,8 @@ router.get('/gen-lat-long', async (req, res) => {
 			let address = i.address.split(' ')
 			console.log('Address')
 			console.log(address)
-			let dawaData = await dawaApi.get(`adresser?vejnavn=${address[0]}&husnr=${address[1]}&struktur=mini`)
+			let dawaData = await dawaApi.get(`adresser?vejnavn=${address[0]}&husnr=${address[1]}&struktur=mini`).then(rs => rs.data)
+			console.log(dawaData[0])
 			if (dawaData[0]) {
 				let dawaAddr = dawaData[0]
 				if (dawaAddr.x && dawaAddr.y) { //lat is y and long is x
@@ -38,13 +39,18 @@ router.get('/gen-lat-long', async (req, res) => {
 						return false
 					}
 				}
+				else return false
 			}
+			else return false
 		}
 		else {
 			return true
 		}
 	}
-	let result = await Promise.all(installations.map(i => editInst(i)))
+	let result = await Promise.all(installations.map(i => {
+		console.log(i.uuid)
+		return editInst(i)
+	}))
 	res.status(200).json(result)
 
 })
